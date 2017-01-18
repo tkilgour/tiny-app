06373184c80141268ab55391798519a727ff6fa0
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-18 11:47:18 -0500

    started tiny-app project

--- express_server.js
+++ express_server.js
@@ -5,6 +5,20 @@ const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+const bodyParser = require('body-parser');
+
+function generateRandomString() {
+  let randomString = "";
+  const dataSet = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for (i = 0; i < 6; i++) {
+    const randomNum = Math.floor(Math.random() * 61);
+    randomString += dataSet[randomNum]
+  };
+  return randomString;
+}
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 
@@ -15,6 +29,13 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.post('/urls/create', (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`http://localhost:8080/${shortURL}`);
 });
 
 app.get('/:id', (req, res) => {
@@ -29,8 +50,17 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/hello', (req, res) => {
   res.end("<html><body>Hello <em>World</em></body></html>\n")
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
